ecfdab182b2e40a1e139c4929e3e2610c74ef9d2
Thenam2kx <thenam2kx@gmail.com>
2024-06-24 20:49:32 +0700

    upload data

---
+++ src/mock-data/QuicklinksData.js
@@ -0,0 +1,41 @@
+export const QuickLinksData = [
+  {
+    "name": "TOP DEAL",
+    "thumbnail_url": "https://salt.tikicdn.com/ts/upload/12/59/f8/b373e84ea401f63cef51ec385bbb9369.png",
+    "url": "https://tiki.vn/khuyen-mai/hero-top-san-pham-ban-chay"
+  },
+  {
+    "name": "Nhập khẩu chính hãng",
+    "thumbnail_url": "https://salt.tikicdn.com/ts/upload/18/ec/0b/7f09f2ec4d8d5ceb81b8c043dbc747d0.png",
+    "url": "https://tiki.vn/khuyen-mai/hang-nhap-khau-chinh-hang"
+  },
+  {
+    "name": "Sản phẩm mới",
+    "thumbnail_url": "https://salt.tikicdn.com/ts/upload/b1/5f/5e/501a9b0ed0c1735d3e3872bad6e587df.png",
+    "url": "https://tiki.vn/khuyen-mai/hang-moi-gia-tot"
+  },
+  {
+    "category_id": 8322,
+    "name": "Nhà Sách Tiki",
+    "thumbnail_url": "https://salt.tikicdn.com/ts/category/ed/20/60/afa9b3b474bf7ad70f10dd6443211d5f.png",
+    "url": "https://tiki.vn/nha-sach-tiki/c8322?itm_campaign=tiki-reco_UNK_DT_UNK_UNK_featured-cate-home_UNK_UNK_UNK_MD_batched_CID.8322&itm_medium=CPC&itm_source=tiki-reco"
+  },
+  {
+    "category_id": 1883,
+    "name": "Nhà Cửa - Đời Sống",
+    "thumbnail_url": "https://salt.tikicdn.com/ts/category/f6/22/46/7e2185d2cf1bca72d5aeac385a865b2b.png",
+    "url": "https://tiki.vn/nha-cua-doi-song/c1883?itm_campaign=tiki-reco_UNK_DT_UNK_UNK_featured-cate-home_UNK_UNK_UNK_MD_batched_CID.1883&itm_medium=CPC&itm_source=tiki-reco"
+  },
+  {
+    "category_id": 1882,
+    "name": "Điện Gia Dụng",
+    "thumbnail_url": "https://salt.tikicdn.com/ts/category/61/d4/ea/e6ea3ffc1fcde3b6224d2bb691ea16a2.png",
+    "url": "https://tiki.vn/dien-gia-dung/c1882?itm_campaign=tiki-reco_UNK_DT_UNK_UNK_featured-cate-home_UNK_UNK_UNK_MD_batched_CID.1882&itm_medium=CPC&itm_source=tiki-reco"
+  },
+  {
+    "category_id": 1789,
+    "name": "Điện Thoại - Máy Tính Bảng",
+    "thumbnail_url": "https://salt.tikicdn.com/ts/category/54/c0/ff/fe98a4afa2d3e5142dc8096addc4e40b.png",
+    "url": "https://tiki.vn/dien-thoai-may-tinh-bang/c1789?itm_campaign=tiki-reco_UNK_DT_UNK_UNK_featured-cate-home_UNK_UNK_UNK_MD_batched_CID.1789&itm_medium=CPC&itm_source=tiki-reco"
+  }
+]
---
+++ src/mock-data/SidebarData.js
@@ -0,0 +1,202 @@
+export const sidebarData = [
+  {
+    "title": "Danh mục",
+    "items": [
+      {
+        "text": "Nhà Sách Tiki",
+        "icon_url": "https://salt.tikicdn.com/ts/category/ed/20/60/afa9b3b474bf7ad70f10dd6443211d5f.png",
+        "link": "https://tiki.vn/nha-sach-tiki/c8322"
+      },
+      {
+        "text": "Nhà Cửa - Đời Sống",
+        "icon_url": "https://salt.tikicdn.com/ts/category/f6/22/46/7e2185d2cf1bca72d5aeac385a865b2b.png",
+        "link": "https://tiki.vn/nha-cua-doi-song/c1883"
+      },
+      {
+        "text": "Điện Thoại - Máy Tính Bảng",
+        "icon_url": "https://salt.tikicdn.com/ts/category/54/c0/ff/fe98a4afa2d3e5142dc8096addc4e40b.png",
+        "link": "https://tiki.vn/dien-thoai-may-tinh-bang/c1789"
+      },
+      {
+        "text": "Đồ Chơi - Mẹ & Bé",
+        "icon_url": "https://salt.tikicdn.com/ts/category/13/64/43/226301adcc7660ffcf44a61bb6df99b7.png",
+        "link": "https://tiki.vn/do-choi-me-be/c2549"
+      },
+      {
+        "text": "Thiết Bị Số - Phụ Kiện Số",
+        "icon_url": "https://salt.tikicdn.com/ts/category/75/34/29/78e428fdd90408587181005f5cc3de32.png",
+        "link": "https://tiki.vn/thiet-bi-kts-phu-kien-so/c1815"
+      },
+      {
+        "text": "Điện Gia Dụng",
+        "icon_url": "https://salt.tikicdn.com/ts/category/61/d4/ea/e6ea3ffc1fcde3b6224d2bb691ea16a2.png",
+        "link": "https://tiki.vn/dien-gia-dung/c1882"
+      },
+      {
+        "text": "Làm Đẹp - Sức Khỏe",
+        "icon_url": "https://salt.tikicdn.com/ts/category/73/0e/89/bf5095601d17f9971d7a08a1ffe98a42.png",
+        "link": "https://tiki.vn/lam-dep-suc-khoe/c1520"
+      },
+      {
+        "text": "Ô Tô - Xe Máy - Xe Đạp",
+        "icon_url": "https://salt.tikicdn.com/ts/category/69/f5/36/c6cd9e2849854630ed74ff1678db8f19.png",
+        "link": "https://tiki.vn/o-to-xe-may-xe-dap/c8594"
+      },
+      {
+        "text": "Thời trang nữ",
+        "icon_url": "https://salt.tikicdn.com/ts/category/55/5b/80/48cbaafe144c25d5065786ecace86d38.png",
+        "link": "https://tiki.vn/thoi-trang-nu/c931"
+      },
+      {
+        "text": "Bách Hóa Online",
+        "icon_url": "https://salt.tikicdn.com/ts/category/40/0f/9b/62a58fd19f540c70fce804e2a9bb5b2d.png",
+        "link": "https://tiki.vn/bach-hoa-online/c4384"
+      },
+      {
+        "text": "Thể Thao - Dã Ngoại",
+        "icon_url": "https://salt.tikicdn.com/ts/category/0b/5e/3d/00941c9eb338ea62a47d5b1e042843d8.png",
+        "link": "https://tiki.vn/the-thao-da-ngoai/c1975"
+      },
+      {
+        "text": "Thời trang nam",
+        "icon_url": "https://salt.tikicdn.com/ts/category/00/5d/97/384ca1a678c4ee93a0886a204f47645d.png",
+        "link": "https://tiki.vn/thoi-trang-nam/c915"
+      },
+      {
+        "text": "Cross Border - Hàng Quốc Tế",
+        "icon_url": "https://salt.tikicdn.com/ts/category/3c/e4/99/eeee1801c838468d94af9997ec2bbe42.png",
+        "link": "https://tiki.vn/cross-border-hang-quoc-te/c17166"
+      },
+      {
+        "text": "Laptop - Máy Vi Tính - Linh kiện",
+        "icon_url": "https://salt.tikicdn.com/ts/category/92/b5/c0/3ffdb7dbfafd5f8330783e1df20747f6.png",
+        "link": "https://tiki.vn/laptop-may-vi-tinh-linh-kien/c1846"
+      },
+      {
+        "text": "Giày - Dép nam",
+        "icon_url": "https://salt.tikicdn.com/ts/category/d6/7f/6c/5d53b60efb9448b6a1609c825c29fa40.png",
+        "link": "https://tiki.vn/giay-dep-nam/c1686"
+      },
+      {
+        "text": "Điện Tử - Điện Lạnh",
+        "icon_url": "https://salt.tikicdn.com/ts/category/c8/82/d4/64c561c4ced585c74b9c292208e4995a.png",
+        "link": "https://tiki.vn/dien-tu-dien-lanh/c4221"
+      },
+      {
+        "text": "Giày - Dép nữ",
+        "icon_url": "https://salt.tikicdn.com/ts/category/cf/ed/e1/5a6b58f21fbcad0d201480c987f8defe.png",
+        "link": "https://tiki.vn/giay-dep-nu/c1703"
+      },
+      {
+        "text": "Máy Ảnh - Máy Quay Phim",
+        "icon_url": "https://salt.tikicdn.com/ts/category/2d/7c/45/e4976f3fa4061ab310c11d2a1b759e5b.png",
+        "link": "https://tiki.vn/may-anh/c1801"
+      },
+      {
+        "text": "Phụ kiện thời trang",
+        "icon_url": "https://salt.tikicdn.com/ts/category/ca/53/64/49c6189a0e1c1bf7cb91b01ff6d3fe43.png",
+        "link": "https://tiki.vn/phu-kien-thoi-trang/c27498"
+      },
+      {
+        "text": "NGON",
+        "icon_url": "https://salt.tikicdn.com/ts/category/1e/8c/08/d8b02f8a0d958c74539316e8cd437cbd.png",
+        "link": "https://tiki.vn/ngon/c44792"
+      },
+      {
+        "text": "Đồng hồ và Trang sức",
+        "icon_url": "https://salt.tikicdn.com/ts/category/8b/d4/a8/5924758b5c36f3b1c43b6843f52d6dd2.png",
+        "link": "https://tiki.vn/dong-ho-va-trang-suc/c8371"
+      },
+      {
+        "text": "Balo và Vali",
+        "icon_url": "https://salt.tikicdn.com/ts/category/3e/c0/30/1110651bd36a3e0d9b962cf135c818ee.png",
+        "link": "https://tiki.vn/balo-va-vali/c6000"
+      },
+      {
+        "text": "Voucher - Dịch vụ",
+        "icon_url": "https://salt.tikicdn.com/ts/category/0a/c9/7b/8e466bdf6d4a5f5e14665ce56e58631d.png",
+        "link": "https://tiki.vn/voucher-dich-vu/c11312"
+      },
+      {
+        "text": "Túi thời trang nữ",
+        "icon_url": "https://salt.tikicdn.com/ts/category/31/a7/94/6524d2ecbec216816d91b6066452e3f2.png",
+        "link": "https://tiki.vn/tui-vi-nu/c976"
+      },
+      {
+        "text": "Túi thời trang nam",
+        "icon_url": "https://salt.tikicdn.com/ts/category/9b/31/af/669e6a133118e5439d6c175e27c1f963.png",
+        "link": "https://tiki.vn/tui-thoi-trang-nam/c27616"
+      },
+      {
+        "text": "Chăm sóc nhà cửa",
+        "icon_url": "https://salt.tikicdn.com/cache/280x280/ts/product/15/d5/1d/64a37269a97a049337a0de82152fd43c.jpg",
+        "link": "https://tiki.vn/cham-soc-nha-cua/c15078"
+      }
+    ]
+  },
+  {
+    "title": "Nổi bật",
+    "items": [
+      {
+        "text": "Tiki ChatGPT",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/a0/c9/78/cddabc413834de509f40455498c7ff47.png",
+        "link": "openChatGPTWidget"
+      },
+      {
+        "text": "Tiki Exchange",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/44/58/fc/804a2dfd610e9075ad5a8f0d13f2b21a.png",
+        "link": "https://exchange.tiki.vn?src=top_desktop_icon"
+      },
+      {
+        "text": "Tốt & Nhanh",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/7e/00/fe/a9798708549255148735ce9406fa7b4d.png",
+        "link": "https://tiki.vn/khuyen-mai/tiki-tot-nhanh?waypoint_id=corevalue"
+      },
+      {
+        "text": "Giá Rẻ Mỗi Ngày",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/ae/72/a3/d4503c3ece932dc8c57d2d5c97cd6ffc.png",
+        "link": "https://tiki.vn/khuyen-mai/re-moi-ngay?src=top_desktop_icon"
+      },
+      {
+        "text": "Xả kho",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/3c/ce/96/db8c083610e45b78d8f7662f0013faa8.png",
+        "link": "https://tiki.vn/khuyen-mai/gia-tot-moi-ngay?src=top_desktop_icon"
+      },
+      {
+        "text": "Mã giảm giá",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/20/68/cf/6d4adbdbcd1c35b0a438a655d9a420d0.png",
+        "link": "https://tiki.vn/khuyen-mai/ma-giam-gia?src=top_desktop_icon"
+      },
+      {
+        "text": "Ưu đãi thẻ, ví",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/1e/27/a7/e2c0e40b6dc45a3b5b0a8e59e2536f23.png",
+        "link": "https://tiki.vn/khuyen-mai/uu-dai-doi-tac?src=top_desktop_icon"
+      },
+      {
+        "text": "Đóng tiền, nạp thẻ",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/4d/a3/cb/c86b6e4f17138195c026437458029d67.png",
+        "link": "https://tiki.vn/san-pham-so?src=top_desktop_icon"
+      },
+      {
+        "text": "Mua trước trả sau",
+        "icon_url": "https://salt.tikicdn.com/ts/tmp/6f/4e/41/93f72f323d5b42207ab851dfa39d44fb.png",
+        "link": "https://tiki.vn/mua-truoc-tra-sau?src=top_desktop_icon"
+      },
+      {
+        "text": "Bảo hiểm Tiki360",
+        "icon_url": "https://salt.tikicdn.com/ts/upload/6f/d0/68/76b6c01998c3f45f70b843c390097690.png",
+        "link": "https://tiki.vn/bao-hiem-so?src=top_desktop_icon"
+      }
+    ]
+  },
+  {
+    "title": "",
+    "items": [
+      {
+        "text": "Bán hàng cùng Tiki",
+        "icon_url": "https://salt.tikicdn.com/cache/100x100/ts/upload/08/2f/14/fd9d34a8f9c4a76902649d04ccd9bbc5.png.webp",
+        "link": ""
+      }
+    ]
+  }
+] 
